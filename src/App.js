94b3bcb761836b9cadd7f0abc1ebc77271ae4498
Him <vuqtpro1592002@gmail.com>
2024-10-24 10:22:46 +0700

    dkmm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import DefaultLayout from './components/layouts/DefaultLayout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>TItok</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route,index)=>{
+            let Layout = DefaultLayout
+            if(route.layout===1){
+              Layout = Fragment
+            }
+
+            const Page = route.component
+            return <Route
+             key={index} 
+             path={route.path} 
+             element={
+              <Layout>
+             <Page></Page>
+             </Layout>
+            }
+             ></Route>
+          })}
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
