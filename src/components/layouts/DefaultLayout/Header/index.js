c39b526706eb3cb55df5c4305b82c9e9bb2423e5
Him <vuqtpro1592002@gmail.com>
2024-11-10 16:51:41 +0700

    s

--- src/components/layouts/DefaultLayout/Header/index.js
+++ src/components/layouts/DefaultLayout/Header/index.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
-import Tippy from "@tippyjs/react/headless";
+import HeadlessTippy from "@tippyjs/react/headless";
+import Tippy from "@tippyjs/react";
+
 import "tippy.js/dist/tippy.css"; // optional
 import images from "../../../../assets/images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -19,6 +21,8 @@ import { Wrapper as PopperWrapper } from "../../../Popper";
 import AccountItem from "../../../AccountItem";
 import Button from "../../../Button";
 import Menu from "../../../Popper/Menu";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { faMessage } from "@fortawesome/free-regular-svg-icons";
 
 const cx = classNames.bind(styles);
 
@@ -59,6 +63,7 @@ function Header() {
       setSearchResult([1, 2, 3]);
     }, 0);
   }, []);
+  const currentUser = 1;
 
   return (
     <header className={cx("wrapper")}>
@@ -67,7 +72,7 @@ function Header() {
           <img src={images.logo} alt="TITOK"></img>
         </div>
 
-        <Tippy
+        <HeadlessTippy
           interactive
           render={(attrs) => (
             <div className={cx("search-result")} tabIndex="-1" {...attrs}>
@@ -95,21 +100,39 @@ function Header() {
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </div>
-        </Tippy>
-        <div className={cx("actions")}>
-          <Button text>UpLoad</Button>
-          <Button
-            primary
-            rightIcon={<FontAwesomeIcon icon={faRightToBracket} />}
-          >
-            Login
-          </Button>
-          <Menu items={Menu_Items}>
-            <button className={cx("more-btn")}>
-              <FontAwesomeIcon icon={faEllipsisVertical} />
+        </HeadlessTippy>
+        {currentUser ? (
+          <div className={cx("current-user")}>
+            <Tippy content="Upload Video" placement="bottom">
+              <Button className={cx("upload-btn")} >
+                <FontAwesomeIcon icon={faPlus} /> Upload
+              </Button>
+            </Tippy>
+
+            <button className={cx("i-mess")}>
+              <FontAwesomeIcon icon={faMessage}></FontAwesomeIcon>
             </button>
-          </Menu>
-        </div>
+            <img
+              src="https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1594805258216454~c5_1080x1080.jpeg?lk3s=a5d48078&nonce=66038&refresh_token=27f2ea86c4707c8f1dbe7d48cabf8802&x-expires=1731384000&x-signature=TF6ApIdLI%2Fj%2FG83MJQpvrTaI2QY%3D&shp=a5d48078&shcp=81f88b70"
+              className={cx("avata-user")}
+            ></img>
+          </div>
+        ) : (
+          <div className={cx("actions")}>
+            <Button text>UpLoad</Button>
+            <Button
+              primary
+              rightIcon={<FontAwesomeIcon icon={faRightToBracket} />}
+            >
+              Login
+            </Button>
+            <Menu items={Menu_Items}>
+              <button className={cx("more-btn")}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            </Menu>
+          </div>
+        )}
       </div>
     </header>
   );
